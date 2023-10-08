fc3b433d8412c247360e7840ec55ecde29b8e11a
Isaac Saunders <IsaacLSaunders@gmail.com>
2023-10-08 12:14:08 -0400

    Isaac: Accountant increment and decrement methods, HappyPath, NegativeNumberInput, NegativeNumberInputs both fail, need fixed in methods

--- CapstoneTests/AcountantTests.cs
+++ CapstoneTests/AcountantTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Capstone.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,10 +9,85 @@ namespace CapstoneTests
     [TestClass]
     public class AcountantTests
     {
-        [TestMethod]
-        public void Accountant_HappyPath()
-        {
 
+        [TestMethod]
+        [DataRow(4, 4.00)]
+        [DataRow(5, 5.00)]
+        [DataRow(5000, 5000.00)]
+        [DataRow(5000, 5000.00)]
+        [DataRow(1/2, 0.00)]
+        [DataRow(int.MaxValue, int.MaxValue)]
+        public void AccountantIncrementBalance_HappyPath(int price, double expected)
+        {
+            //arrange 
+            Accountant sut = new Accountant();
+            decimal expectedAsDecimal = Convert.ToDecimal(expected);
+
+            //act
+            sut.IncrementBalance(price);
+
+            //assert
+            Assert.AreEqual(sut.Balance, expectedAsDecimal);
         }
+
+        [TestMethod]
+        [DataRow(int.MinValue, 0.00)]
+        [DataRow(-3, 0.00)]
+        [DataRow(-6, 0.00)]
+        [DataRow(-1000, 0.00)]
+        public void AccountantIncrementBalance_NegativeNumberInput(int price, double expected)
+        {
+            //arrange 
+            Accountant sut = new Accountant();
+            decimal expectedAsDecimal = Convert.ToDecimal(expected);
+
+            //act
+            sut.IncrementBalance(price);
+
+            //assert
+            Assert.AreEqual(sut.Balance, expectedAsDecimal);
+        }
+        
+
+        [TestMethod]
+        [DataRow(4, (double)(int.MaxValue - 4))]
+        [DataRow(5, (double)(int.MaxValue - 5))]
+        [DataRow(5000, (double)(int.MaxValue - 5000))]
+        [DataRow(1/2, (double)(int.MaxValue))]
+        [DataRow(int.MaxValue, 0)]
+        public void AccountantDecrementBalance_HappyPath(int price, double expected)
+        {
+            //arrange 
+            Accountant sut = new Accountant();
+            sut.IncrementBalance(Convert.ToDecimal(int.MaxValue));
+            decimal expectedAsDecimal = Convert.ToDecimal(expected);
+
+            //act
+            sut.DecrementBalance(price);
+
+            //assert
+            Assert.AreEqual(sut.Balance, expectedAsDecimal);
+        }
+        
+        [TestMethod]
+
+        [DataRow(int.MinValue, 0)]
+        [DataRow(-3, 0)]
+        [DataRow(-6, 0)]
+        [DataRow(-1000, 0)]
+        public void AccountantDecrementBalance_NegativeNumberInput(int price, double expected)
+        {
+            //arrange 
+            Accountant sut = new Accountant();
+            sut.IncrementBalance(Convert.ToDecimal(int.MaxValue));
+            decimal expectedAsDecimal = Convert.ToDecimal(expected);
+
+            //act
+            sut.DecrementBalance(price);
+
+            //assert
+            Assert.AreEqual(sut.Balance, expectedAsDecimal);
+        }
+
     }
 }
