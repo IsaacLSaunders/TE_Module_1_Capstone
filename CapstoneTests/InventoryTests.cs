dbadcc80006f8cd54aff029b5c5d2532aef00374
Ifeyinwa Uwazie <icu2065@gmail.com>
2023-10-10 15:35:32 -0400

    Added VendingMachine & Inventory Tests and deleted UI and Menu tests

--- CapstoneTests/InventoryTests.cs
+++ CapstoneTests/InventoryTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Capstone.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,10 +10,62 @@ namespace CapstoneTests
 
     public class InventoryTests
     {
-        [TestMethod]
-        public void Inventory_HappyPath()
-        {
 
-        }
+        //[TestMethod]
+        //[DataRow("b2")]
+        //[DataRow("a2")]
+        //[DataRow("b1")]
+        //[DataRow("B3")]
+        //public void ItemDecrementInventoryDecrement_HappyPath(string id) //testing that our items are removed from stock appropriately
+        //{
+
+        //    //Arrange
+        //    VMEmployee employee = new VMEmployee();
+        //    VendingMachine vendingMachine = new VendingMachine(employee.StockVM());
+
+        //    VMEmployee employeeTest = new VMEmployee();
+        //    VendingMachine vendingMachineTest = new VendingMachine(employee.StockVM());
+
+        //    //Act 
+        //        //in one dictionary, remove an item in the list of items manually using the List.Remove method
+        //        //in the other dictionary remove an item using our decrementInventory method, passing in the id
+        //            //once this is completed
+
+        //    //Assert
+        //        //compare the values in the two dictionaries
+        //            //CollectionAssert.AreEquivalent? not sure
+        //                //assert.areEqual count of each list at the id in the dictionary?
+
+
+        //}
+
+
+        //[TestMethod]
+        ////change these to bad values
+        ////[DataRow("2", )]
+        ////[DataRow("", )]
+        ////[DataRow("J1", )]
+        ////[DataRow("v3", )]
+        //public void ItemDecrementInventoryDecrement_InvalidId(string id) //testing that our items are removed from stock appropriately 
+        //{ //WE DO NOT GET TO THE ITEMDECREMENTINVENTORY METHOD IF WE DO NOT HAVE VALID INPUT FOR SELECTITEMBYCODEMETHOD. THIS IS TESTED BY SELECTITEMBYCODE TESTS
+
+        //    //Arrange
+        //    VMEmployee employee = new VMEmployee();
+        //    VendingMachine vendingMachine = new VendingMachine(employee.StockVM());
+
+        //    VMEmployee employeeTest = new VMEmployee();
+        //    VendingMachine vendingMachineTest = new VendingMachine(employee.StockVM());
+
+        //    //Act 
+        //    //in one dictionary, remove an item in the list of items manually using the List.Remove method
+        //    //in the other dictionary remove an item using our decrementInventory method, passing in the id
+        //    //once this is completed
+
+        //    //Assert
+        //    //compare the values in the two dictionaries
+        //    //CollectionAssert.AreEquivalent? not sure
+        //    //assert.areEqual count of each list at the id in the dictionary?
+
+        //}
     }
 }
--- CapstoneTests/VendingMachineTests.cs
+++ CapstoneTests/VendingMachineTests.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Capstone.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace CapstoneTests
@@ -10,9 +12,100 @@ namespace CapstoneTests
     public class VendingMachineTests
     {
         [TestMethod]
-        public void VendingMachine_HappyPath()
+        [DataRow("a2", true)]
+        [DataRow("C1", true)]
+        [DataRow("D4", true)]
+        [DataRow("b3", true)]
+        [DataRow("A2", true)]
+        public void SelectItemByCode_HappyPath(string input, bool expected)
         {
+            //Arrange
+            VMEmployee employee = new VMEmployee();
+            VendingMachine vendingMachine = new VendingMachine(employee.StockVM());
+            VendingMachine sut = new VendingMachine(employee.StockVM());
+
+
+            //Act
+            bool actual = sut.SelectItemByCode(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        [DataRow("z2", false)]
+        [DataRow("E1", false)]
+        [DataRow("K71", false)]
+        [DataRow("H", false)]
+        [DataRow("0", false)]
+
+        public void SelectItemByCode_InvalidId(string input, bool expected)
+        {
+            //Arrange
+            VMEmployee employee = new VMEmployee();
+            VendingMachine vendingMachine = new VendingMachine(employee.StockVM());
+            VendingMachine sut = new VendingMachine(employee.StockVM());
+
+            //Act
+            bool actual = sut.SelectItemByCode(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "That is not a valid input.");
+        }
+
+
+        [TestMethod]
+
+        public void SelectItemByCode_NullOrEmptyId()
+        {
+            //Arrange
+            VMEmployee employee = new VMEmployee();
+            VendingMachine vendingMachine = new VendingMachine(employee.StockVM());
+            VendingMachine sut = new VendingMachine(employee.StockVM());
+
+            string input = null;
+            bool expected = false;
+
+            string input1 = "";
+            bool expected1 = false;
+
+            //Act
+            bool actual = sut.SelectItemByCode(input);
+            bool actual1 = sut.SelectItemByCode(input1);
+
+            //Assert
+            Assert.AreEqual(expected, actual, "That is not a valid input.");
+            Assert.AreEqual(expected1, actual1, "That is not a valid input.");
+        }
+
+
+        [TestMethod]
+        [DataRow("D3", true)]
+        [DataRow("D3", true)]
+        [DataRow("D3", true)]
+        [DataRow("d3", true)]
+        [DataRow("d3", true)]
+        [DataRow("d3", true)] //Set to false as no 6th item (sold out)?
+
+        public void SelectItemByCode_SoldOut(string input, bool expected) //This will break!
+        {
+            //Arrange
+            VMEmployee employee = new VMEmployee();
+            VendingMachine vendingMachine = new VendingMachine(employee.StockVM());
+            VendingMachine sut = new VendingMachine(employee.StockVM());
+
+
+            //Act
+
+            bool actual = sut.SelectItemByCode(input);
+
+
+            //Assert
+            Assert.AreEqual(expected, actual, "This item is out of stock.");
+
 
         }
     }
 }
+
